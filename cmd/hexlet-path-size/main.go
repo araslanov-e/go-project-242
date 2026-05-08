79c0b24ff742480eecca9c14cf9ccc1a72a635ff
Araslanov Evgeny <araslanov_e_i@list.ru>
2026-05-08 16:36:32 +0500

    Add CLI entrypoint for path size output

--- cmd/hexlet-path-size/main.go
+++ cmd/hexlet-path-size/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"code"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Print("Hello from Hexlet!")
+	path := "."
+	if len(os.Args) > 1 {
+		path = os.Args[1]
+	}
+
+	res, err := code.GetPathSize(path)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("%s\t%s\n", res, path)
 }
